e6ac3eb669d82772edd6f380a4a35f40c2a36b19
teo <yesayateofilushendrawan@gmail.com>
2026-06-02 10:28:09 +0700

    fix: fix classes migration

--- database/migrations/2026_06_02_030745_create_classes_table.php
+++ database/migrations/2026_06_02_030745_create_classes_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('classes', function (Blueprint $table) {
             $table->id();
             $table->string('class_name');
+            $table->string('class_code')->unique();
             $table->timestamps();
             $table->softDeletes();
         });
